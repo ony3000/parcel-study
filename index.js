dadd48f468cd00aef3fe29705f87747e07ccd909
Hyeonjong <nianelo4@gmail.com>
2018-08-04 10:36:23 +0900

    Apply module import

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello world!');
+import _ from 'lodash';
+
+function component() {
+  var element = document.createElement('div');
+
+  element.innerHTML = _.join([
+    'This is',
+    'personal page',
+    'to studying',
+    'parcel bundler!',
+  ], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
