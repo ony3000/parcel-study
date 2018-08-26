7d8599ce7bfec10c55a5f6dccceca137af9846a0
Hyeonjong <nianelo4@gmail.com>
2018-08-26 14:05:35 +0900

    Relocate elements

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,10 @@
 <template>
     <section id="app">
         <p>Hello Vue!</p>
+        <button class="button is-primary">
+            <span class="mdi mdi-download"></span>
+            <span>Load async component</span>
+        </button>
     </section>
 </template>
 
