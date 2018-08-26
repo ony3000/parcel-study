aa093d4b264e47c3a07c10ff8ec75b13d7116b00
Hyeonjong <nianelo4@gmail.com>
2018-08-26 14:12:37 +0900

    Add component which will import dynamically

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,15 +1,29 @@
 <template>
     <section id="app">
         <p>Hello Vue!</p>
-        <button class="button is-primary">
+        <button class="button is-primary" @click="loadFirst">
             <span class="mdi mdi-download"></span>
             <span>Load async component</span>
         </button>
+        <addon-first v-if="isTriggered"/>
     </section>
 </template>
 
 <script>
 export default {
     name: 'app',
+    components: {
+        AddonFirst: () => import('./addon-first').then(module => module.default),
+    },
+    data() {
+        return {
+            isTriggered: false,
+        };
+    },
+    methods: {
+        loadFirst() {
+            this.isTriggered = true;
+        },
+    },
 };
 </script>
---
+++ src/components/addon-first.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>I'm the first async component!</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'addon-first',
+};
+</script>
